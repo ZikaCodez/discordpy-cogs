26f811fa0ac373a4b94733c148bb8050ab1a88b6
Scoopy <70343014+ScopesCodez@users.noreply.github.com>
2021-09-22 03:08:36 +0200

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,19 @@
+import discord
+import os
+from discord.ext import commands
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+TOKEN = "" # Insert your bot's token here.
+
+@bot.event
+async def on_ready():
+  for cog in os.listdir(r"cogs"): # Loop through each file in your "cogs" directory.
+      if cog.endswith(".py"):
+          try:
+              cog = f"cogs.{cog.replace('.py', '')}"
+              bot.load_extension(cog) # Load the file as an extension.
+          except Exception as e:
+              print(f"{cog} is failed to load:")
+              raise e
+    print(f"Logged in as {bot.user}")
+    
+bot.run(TOKEN)
